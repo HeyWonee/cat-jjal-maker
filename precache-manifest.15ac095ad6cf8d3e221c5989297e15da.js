c7af56f77c4b6b4a22cbbe4e3d0c0cc3ca4b53dd
신혜원 <hehe2you@gmail.com>
2022-06-10 15:48:41 +0900

    Updates

---
+++ precache-manifest.15ac095ad6cf8d3e221c5989297e15da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c755b761a3571bcb247b",
+    "url": "/static/css/main.e3b1266f.chunk.css"
+  },
+  {
+    "revision": "c755b761a3571bcb247b",
+    "url": "/static/js/main.c755b761.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "ad72a69e96cddbf8cdc6",
+    "url": "/static/js/2.ad72a69e.chunk.js"
+  },
+  {
+    "revision": "a850c70773011bd5f111",
+    "url": "/static/js/runtime~main.a850c707.js"
+  },
+  {
+    "revision": "2f5e4024d90965b9c0de486729ad3224",
+    "url": "/index.html"
+  }
+];
